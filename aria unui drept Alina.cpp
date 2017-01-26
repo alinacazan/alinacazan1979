6203dfba5ef2c728982c529e93ae4bbe97ed692d
alinacazan <alinacazan79@yahoo.com>
2017-01-26 08:23:57 +0200

    Add files via upload

---
+++ aria unui drept Alina.cpp
@@ -0,0 +1,15 @@
+//aria unui dreptunghi cu laturile a,b
+//A=a*b
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double a, b;
+    
+    cout<<"Lungimea este egala cu: "; cin>>a; cout<< endl;
+    cout<<"Latimea este egala cu: "; cin>>b; cout<< endl;
+    cout<<"Aria este egala cu: "<<a*b<<".";
+    system("pause");
+}
+
